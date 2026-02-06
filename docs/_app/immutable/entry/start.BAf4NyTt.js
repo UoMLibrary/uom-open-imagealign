625558139e798055664862e15aab45c867c7ceec
Christopher Wilson <chris@abitofcode.com>
2026-02-06 15:27:29 +0000

    Added page title and favicon

--- docs/_app/immutable/entry/start.BAf4NyTt.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/xoaQOSdn.js";export{o as load_css,r as start};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,23 @@
 	import MainVIew from '$lib/ImageAlign/MainView.svelte';
 </script>
 
-<MainVIew />
+<main>
+	<MainVIew />
+</main>
+
+<svelte:head>
+	<title>Image Alignment Tool</title>
+	<meta
+		name="description"
+		content="Experimental image alignment and registration tool for research, exploration, and prototyping workflows."
+	/>
+</svelte:head>
+
+<style>
+	main {
+		position: relative;
+		height: 100%;
+		width: 100%;
+		overflow: hidden;
+	}
+</style>
